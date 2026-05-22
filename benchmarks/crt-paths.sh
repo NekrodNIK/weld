fe5a01962b5e5083efcd83f9630323b4bed9c791
NekrodNIK <60639354+NekrodNIK@users.noreply.github.com>
2026-05-22 23:20:32 +0700

    fix paths

--- benchmarks/crt-paths.sh
+++ benchmarks/crt-paths.sh
@@ -1,8 +1,8 @@
-CRT1=/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/crt1.o
-CRTI=/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/crti.o
-CRTN=/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/crtn.o
-CRTBEGIN=/usr/lib/gcc/x86_64-redhat-linux/16/crtbegin.o
-CRTEND=/usr/lib/gcc/x86_64-redhat-linux/16/crtend.o
-LIBC=/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libc.a
-LIBGCC=/usr/lib/gcc/x86_64-redhat-linux/16/libgcc.a
-LIBGCC_EH=/usr/lib/gcc/x86_64-redhat-linux/16/libgcc_eh.a
+CRT1=./libs/crt1.o
+CRTI=./libs/crti.o
+CRTN=./libs/crtn.o
+CRTBEGIN=./libs/crtbegin.o
+CRTEND=./libs/crtend.o
+LIBC=./libs/libc.a
+LIBGCC=./libs/libgcc.a
+LIBGCC_EH=./libs/libgcc_eh.a
